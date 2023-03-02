9c5d185d32156768b7ca12217fa817811b13c5ce
mantr88 <109439896+mantr88@users.noreply.github.com>
2023-03-02 20:05:23 +0000

    Deploying to gh-pages from @ mantr88/goit-js-hw-09@fa26d83a88d8118b47e88757b86674866d92715a 🚀

--- 01-color-switcher.539196b8.js
+++
@@ -1,2 +0,0 @@
-console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA");
-//# sourceMappingURL=01-color-switcher.539196b8.js.map
--- 01-color-switcher.a0ee02b4.js
+++
@@ -1,2 +0,0 @@
-console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA"),console.log("AAAAA");
-//# sourceMappingURL=01-color-switcher.a0ee02b4.js.map
